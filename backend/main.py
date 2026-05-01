c6f154bb337d445ef868dab8265ec49a77d6844c
Abay-Wizard <abaytessema251@gmail.com>
2026-05-01 17:54:03 +0200

    cors issue solved

--- backend/main.py
+++ backend/main.py
@@ -36,7 +36,7 @@ app = FastAPI(title="Market Data Dashboard API", lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://flowx-flax.vercel.app", "http://localhost:3000"],
+    allow_origins=["https://flowx-a.vercel.app/login", "http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
